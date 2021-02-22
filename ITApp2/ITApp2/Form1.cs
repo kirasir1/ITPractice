5f930dfaea58b4307af3dbac57a49ce035ee9a07
kirasir1 <zebra46@gmail.com>
2021-02-22 10:16:33 +0300

    Задание 2

--- ITApp2/ITApp2/Form1.cs
+++ ITApp2/ITApp2/Form1.cs
@@ -12,6 +12,59 @@ namespace ITApp2
 {
   public partial class Form1 : Form
   {
+    Random randevent = new Random();
+    int event1, event2, event3, event4 = 0;
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      event1 = randevent.Next(0, 4);
+      if (event1 == 3)
+      {
+        label1.Text = " ";
+      }
+      if (label1.Text == " " && label2.Text == " " && label1.Text == " " && label1.Text == " ")
+      {
+        label5.Text = "Ты выиграл!";
+
+      }
+    }
+    private void button2_Click(object sender, EventArgs e)
+    {
+      event2 = randevent.Next(0, 4);
+      if (event2 == 3)
+      {
+        label2.Text = " ";
+      }
+      if (label1.Text == " " && label2.Text == " " && label3.Text == " " && label4.Text == " ")
+      {
+        label5.Text = "Ты выиграл!";
+      }
+  }
+    private void button3_Click(object sender, EventArgs e)
+    {
+      event3 = randevent.Next(0, 4);
+      if (event3 == 3)
+      {
+        label3.Text = " ";
+      }
+      if (label1.Text == " " && label2.Text == " " && label3.Text == " " && label4.Text == " ")
+      {
+        label5.Text = "Ты выиграл!";
+      }
+    }
+    private void button4_Click(object sender, EventArgs e)
+    {
+      event4 = randevent.Next(0, 4);
+      if (event4 == 3)
+      {
+        label4.Text = " ";
+      }
+      if (label1.Text == " " && label2.Text == " " && label3.Text == " " && label4.Text == " ")
+      {
+        label5.Text = "Ты выиграл!";
+      }
+    }
+
     public Form1()
     {
       InitializeComponent();
