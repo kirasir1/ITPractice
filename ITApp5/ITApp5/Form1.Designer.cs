b6885ebbc7b9a0aa6ea28a43081d60e1ab72a7f1
kerbal <zebra46@gmail.com>
2021-02-24 20:31:06 +0300

    Вторая практика, 1 задание

--- ITApp5/ITApp5/Form1.Designer.cs
+++ ITApp5/ITApp5/Form1.Designer.cs
@@ -30,11 +30,11 @@
     {
       this.label1 = new System.Windows.Forms.Label();
       this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.radioButton3 = new System.Windows.Forms.RadioButton();
+      this.radioButton2 = new System.Windows.Forms.RadioButton();
+      this.radioButton1 = new System.Windows.Forms.RadioButton();
       this.textBox1 = new System.Windows.Forms.TextBox();
       this.button1 = new System.Windows.Forms.Button();
-      this.radioButton1 = new System.Windows.Forms.RadioButton();
-      this.radioButton2 = new System.Windows.Forms.RadioButton();
-      this.radioButton3 = new System.Windows.Forms.RadioButton();
       this.richTextBox1 = new System.Windows.Forms.RichTextBox();
       this.groupBox1.SuspendLayout();
       this.SuspendLayout();
@@ -60,6 +60,39 @@
       this.groupBox1.TabStop = false;
       this.groupBox1.Text = "Выберите функцию f(x)";
       // 
+      // radioButton3
+      // 
+      this.radioButton3.AutoSize = true;
+      this.radioButton3.Location = new System.Drawing.Point(7, 90);
+      this.radioButton3.Name = "radioButton3";
+      this.radioButton3.Size = new System.Drawing.Size(42, 17);
+      this.radioButton3.TabIndex = 2;
+      this.radioButton3.TabStop = true;
+      this.radioButton3.Text = "exp";
+      this.radioButton3.UseVisualStyleBackColor = true;
+      // 
+      // radioButton2
+      // 
+      this.radioButton2.AutoSize = true;
+      this.radioButton2.Location = new System.Drawing.Point(7, 55);
+      this.radioButton2.Name = "radioButton2";
+      this.radioButton2.Size = new System.Drawing.Size(48, 17);
+      this.radioButton2.TabIndex = 1;
+      this.radioButton2.TabStop = true;
+      this.radioButton2.Text = "ch(x)";
+      this.radioButton2.UseVisualStyleBackColor = true;
+      // 
+      // radioButton1
+      // 
+      this.radioButton1.AutoSize = true;
+      this.radioButton1.Location = new System.Drawing.Point(7, 20);
+      this.radioButton1.Name = "radioButton1";
+      this.radioButton1.Size = new System.Drawing.Size(47, 17);
+      this.radioButton1.TabIndex = 0;
+      this.radioButton1.TabStop = true;
+      this.radioButton1.Text = "sh(x)";
+      this.radioButton1.UseVisualStyleBackColor = true;
+      // 
       // textBox1
       // 
       this.textBox1.Location = new System.Drawing.Point(152, 78);
@@ -75,39 +108,7 @@
       this.button1.TabIndex = 3;
       this.button1.Text = "Решить";
       this.button1.UseVisualStyleBackColor = true;
-      // 
-      // radioButton1
-      // 
-      this.radioButton1.AutoSize = true;
-      this.radioButton1.Location = new System.Drawing.Point(7, 20);
-      this.radioButton1.Name = "radioButton1";
-      this.radioButton1.Size = new System.Drawing.Size(85, 17);
-      this.radioButton1.TabIndex = 0;
-      this.radioButton1.TabStop = true;
-      this.radioButton1.Text = "radioButton1";
-      this.radioButton1.UseVisualStyleBackColor = true;
-      // 
-      // radioButton2
-      // 
-      this.radioButton2.AutoSize = true;
-      this.radioButton2.Location = new System.Drawing.Point(7, 55);
-      this.radioButton2.Name = "radioButton2";
-      this.radioButton2.Size = new System.Drawing.Size(85, 17);
-      this.radioButton2.TabIndex = 1;
-      this.radioButton2.TabStop = true;
-      this.radioButton2.Text = "radioButton2";
-      this.radioButton2.UseVisualStyleBackColor = true;
-      // 
-      // radioButton3
-      // 
-      this.radioButton3.AutoSize = true;
-      this.radioButton3.Location = new System.Drawing.Point(7, 90);
-      this.radioButton3.Name = "radioButton3";
-      this.radioButton3.Size = new System.Drawing.Size(85, 17);
-      this.radioButton3.TabIndex = 2;
-      this.radioButton3.TabStop = true;
-      this.radioButton3.Text = "radioButton3";
-      this.radioButton3.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
       // 
       // richTextBox1
       // 
@@ -121,7 +122,7 @@
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.ClientSize = new System.Drawing.Size(770, 447);
       this.Controls.Add(this.richTextBox1);
       this.Controls.Add(this.button1);
       this.Controls.Add(this.textBox1);
--- ITApp5/ITApp5/Form1.cs
+++ ITApp5/ITApp5/Form1.cs
@@ -16,5 +16,55 @@ namespace ITApp5
     {
       InitializeComponent();
     }
+    // Индивидуальное задание 3
+    private void button1_Click(object sender, EventArgs e)
+    {
+      double x = Convert.ToInt32(textBox1.Text);
+      if (radioButton1.Checked == true)
+      {
+        if (x < 0)
+        {
+          richTextBox1.Text += "Ответ: -1\n";
+        }
+        else if (x>=0 && x < 5)
+        {
+          richTextBox1.Text += "Ответ: " + Convert.ToString(2*Math.Sinh(x))+ "\n";
+        }
+        else if (x >= 5)
+        {
+          richTextBox1.Text += "Ответ: " + Convert.ToString(Math.Sinh(x)) + "\n";
+        }
+      }
+      if (radioButton2.Checked == true)
+      {
+        if (x < 0)
+        {
+          richTextBox1.Text += "Ответ: -1\n";
+        }
+        else if (x >= 0 && x < 5)
+        {
+          richTextBox1.Text += "Ответ: " + Convert.ToString(2 * Math.Cosh(x)) + "\n";
+        }
+        else if (x >= 5)
+        {
+          richTextBox1.Text += "Ответ: " + Convert.ToString(Math.Cosh(x)) + "\n";
+        }
+      }
+      if (radioButton3.Checked == true)
+      {
+        if (x < 0)
+        {
+          richTextBox1.Text += "Ответ: -1\n";
+        }
+        else if (x >= 0 && x < 5)
+        {
+          richTextBox1.Text += "Ответ: " + Convert.ToString(2 * Math.Exp(x)) + "\n";
+        }
+        else if (x >= 5)
+        {
+          richTextBox1.Text += "Ответ: " + Convert.ToString(Math.Exp(x)) + "\n";
+        }
+      }
+    }
   }
 }
