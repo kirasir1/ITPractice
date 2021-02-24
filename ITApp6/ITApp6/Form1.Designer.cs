5215434d539e0ff910272d50ecacb9e0087d571c
kerbal <zebra46@gmail.com>
2021-02-24 23:27:50 +0300

    Новое задание

---
+++ ITApp6/ITApp6/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace ITApp6
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(127, 90);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(200, 20);
+      this.textBox1.TabIndex = 0;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(30, 93);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(61, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Введите n:";
+      // 
+      // Form1
+      // 
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+      this.AutoScroll = true;
+      this.ClientSize = new System.Drawing.Size(841, 441);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.textBox1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label label1;
+  }
+}
+
