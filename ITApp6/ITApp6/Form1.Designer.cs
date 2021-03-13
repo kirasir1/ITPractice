1040fdbf8bffeeed0dc5e4ef8b0346e7e614a64c
Шарапов Кирилл <kirasir2001@hotmail.com>
2021-03-13 10:48:28 +0300

    Практика 4

--- ITApp6/ITApp6/Form1.Designer.cs
+++ ITApp6/ITApp6/Form1.Designer.cs
@@ -31,11 +31,17 @@ namespace ITApp6
     {
       this.textBox1 = new System.Windows.Forms.TextBox();
       this.label1 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+      this.radioButton1 = new System.Windows.Forms.RadioButton();
+      this.radioButton2 = new System.Windows.Forms.RadioButton();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.groupBox1.SuspendLayout();
       this.SuspendLayout();
       // 
       // textBox1
       // 
-      this.textBox1.Location = new System.Drawing.Point(127, 90);
+      this.textBox1.Location = new System.Drawing.Point(118, 86);
       this.textBox1.Name = "textBox1";
       this.textBox1.Size = new System.Drawing.Size(200, 20);
       this.textBox1.TabIndex = 0;
@@ -43,21 +49,77 @@ namespace ITApp6
       // label1
       // 
       this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(30, 93);
+      this.label1.Location = new System.Drawing.Point(21, 89);
       this.label1.Name = "label1";
       this.label1.Size = new System.Drawing.Size(61, 13);
       this.label1.TabIndex = 1;
       this.label1.Text = "Введите n:";
       // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(347, 84);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 2;
+      this.button1.Text = "Решить";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // richTextBox1
+      // 
+      this.richTextBox1.Location = new System.Drawing.Point(24, 200);
+      this.richTextBox1.Name = "richTextBox1";
+      this.richTextBox1.Size = new System.Drawing.Size(648, 260);
+      this.richTextBox1.TabIndex = 3;
+      this.richTextBox1.Text = "";
+      // 
+      // radioButton1
+      // 
+      this.radioButton1.AutoSize = true;
+      this.radioButton1.Location = new System.Drawing.Point(6, 19);
+      this.radioButton1.Name = "radioButton1";
+      this.radioButton1.Size = new System.Drawing.Size(86, 17);
+      this.radioButton1.TabIndex = 4;
+      this.radioButton1.TabStop = true;
+      this.radioButton1.Text = "Сумма ряда";
+      this.radioButton1.UseVisualStyleBackColor = true;
+      // 
+      // radioButton2
+      // 
+      this.radioButton2.AutoSize = true;
+      this.radioButton2.Location = new System.Drawing.Point(6, 56);
+      this.radioButton2.Name = "radioButton2";
+      this.radioButton2.Size = new System.Drawing.Size(126, 17);
+      this.radioButton2.TabIndex = 5;
+      this.radioButton2.TabStop = true;
+      this.radioButton2.Text = "Произведение ряда";
+      this.radioButton2.UseVisualStyleBackColor = true;
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.radioButton1);
+      this.groupBox1.Controls.Add(this.radioButton2);
+      this.groupBox1.Location = new System.Drawing.Point(472, 59);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(200, 81);
+      this.groupBox1.TabIndex = 6;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Выберите действие";
+      // 
       // Form1
       // 
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
       this.AutoScroll = true;
-      this.ClientSize = new System.Drawing.Size(841, 441);
+      this.ClientSize = new System.Drawing.Size(693, 520);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.richTextBox1);
+      this.Controls.Add(this.button1);
       this.Controls.Add(this.label1);
       this.Controls.Add(this.textBox1);
       this.Name = "Form1";
       this.Text = "Form1";
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -67,6 +129,11 @@ namespace ITApp6
 
     private System.Windows.Forms.TextBox textBox1;
     private System.Windows.Forms.Label label1;
-  }
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
 }
 
--- ITApp6/ITApp6/Form1.cs
+++ ITApp6/ITApp6/Form1.cs
@@ -16,5 +16,75 @@ namespace ITApp6
     {
       InitializeComponent();
     }
+    //Индивидуальное задание 2
+        private void button1_Click(object sender, EventArgs e)
+        {
+      double n = Convert.ToDouble(textBox1.Text);
+      if (radioButton1.Checked)
+      {
+        double res = Summing(n);
+        richTextBox1.Text += "Сумма ряда равна " + res.ToString() + "\n";
+      }
+      else if (radioButton2.Checked)
+      {
+        double res = Summing2(n);
+        richTextBox1.Text += "Произведение ряда равно " + res.ToString() + "\n";
+      }
+        }
+    static int Fact(int x)
+    {
+      if (x == 0)
+      {
+        return 1;
+      }
+      else
+      {
+        return x * Fact(x - 1);
+      }
+    }
+    public double Summing(double x)
+    {
+      double sum = 0;
+      double sum1 = 0;
+      if (x < 1)
+      {
+        for (int i = 1; (1 / (2 * i + Fact(i))) <= x; i++)
+        {
+          sum1 = (2 * i) + Fact(i);
+          sum = sum + (1 / sum1);
+        }
+      }
+      if (x >= 1)
+      {
+        for (int i = 1; i <= x; i++)
+        {
+          sum1 = (2 * i) + Fact(i);
+          sum = sum + (1 / sum1);
+        }
+      }
+      return sum;
+    }
+    public double Summing2(double x)
+    {
+      double sum = 1;
+      double sum1 = 0;
+      if (x < 1)
+      {
+        for (int i = 1; (1 / (2 * i + Fact(i))) <= x; i++)
+        {
+          sum1 = (2 * i) + Fact(i);
+          sum *= (1 / sum1);
+        }
+      }
+      if (x >= 1)
+      {
+        for (int i = 1; i <= x; i++)
+        {
+          sum1 = (2 * i) + Fact(i);
+          sum = sum * (1 / sum1);
+        }
+      }
+      return sum;
+    }
   }
 }
