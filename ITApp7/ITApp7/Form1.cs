a046e9716f8ec8770fd6e491dedaed1afaa81b6c
Шарапов Кирилл <kirasir2001@hotmail.com>
2021-03-13 11:33:56 +0300

    Практическое задание 2 2

---
+++ ITApp7/ITApp7/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITApp7
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+   private void button1_Click(object sender, EventArgs e)
+   {
+      listBox1.Items.Add(textBox1.Text);
+      textBox1.Text = "";
+   }
+    //Индивидуальное задание 5
+    private void button2_Click(object sender, EventArgs e)
+    {
+      int number = listBox1.SelectedIndex;
+      string word = (string)listBox1.Items[number];
+      label3.Text = "Полученное слово: " + Reverse(word);
+    }
+    public static string Reverse(string s)
+    {
+      char[] charArray = s.ToCharArray();
+      Array.Reverse(charArray);
+      return new string(charArray);
+    }
+
+  }
+}
