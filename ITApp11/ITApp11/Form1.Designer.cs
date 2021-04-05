f60850828b2b5a0c677af7c435d857a5570d955b
Шарапов Кирилл <kirasir2001@hotmail.com>
2021-04-05 11:28:50 +0300

    Продолжение работы №9

--- ITApp11/ITApp11/Form1.Designer.cs
+++ ITApp11/ITApp11/Form1.Designer.cs
@@ -28,13 +28,25 @@
     /// </summary>
     private void InitializeComponent()
     {
+      this.button1 = new System.Windows.Forms.Button();
       this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(175, 541);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(135, 23);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Сделать навал дыма";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(936, 537);
+      this.ClientSize = new System.Drawing.Size(547, 576);
+      this.Controls.Add(this.button1);
       this.Name = "Form1";
       this.Text = "Form1";
       this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
@@ -42,7 +54,9 @@
 
     }
 
-    #endregion
-  }
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
 }
 
--- ITApp11/ITApp11/Form1.cs
+++ ITApp11/ITApp11/Form1.cs
@@ -17,14 +17,57 @@ namespace ITApp11
     {
       InitializeComponent();
     }
-
+    bool flag = false;
     private void Form1_Paint(object sender, PaintEventArgs e)
     {
+      Pen blackPen = new Pen(Color.Black, 3);
       Graphics painting = e.Graphics;
       painting.Clear(Color.White);
-      Pen blackPen = new Pen(Color.Black, 3);
-      Rectangle rect = new Rectangle(0, 0, 200, 100);
-      e.Graphics.DrawRectangle(blackPen, 0, 0, 500, 500);
+      SolidBrush blueBrush = new SolidBrush(Color.LightSkyBlue);
+      Rectangle rect = new Rectangle(10, 10, 500, 500);
+      e.Graphics.FillRectangle(blueBrush, rect);
+      SolidBrush sunBrush = new SolidBrush(Color.Yellow);
+      Rectangle r = new Rectangle(40, 40, 75, 75);
+      e.Graphics.FillEllipse(sunBrush, r);
+      SolidBrush brownBrush = new SolidBrush(Color.SaddleBrown);
+      Rectangle houseNew = new Rectangle(150, 310, 200, 200);
+      e.Graphics.DrawRectangle(blackPen, 150, 310, 200, 200);
+      e.Graphics.FillRectangle(brownBrush, houseNew);
+      Point point1 = new Point(250, 180);
+      Point point2 = new Point(150, 310);
+      Point point3 = new Point(350, 310);
+      Point[] curvePoints = {point1, point2, point3};
+      e.Graphics.DrawPolygon(blackPen, curvePoints);
+      e.Graphics.FillPolygon(brownBrush, curvePoints);  
+      SolidBrush windowBrush = new SolidBrush(Color.LightGray);
+      Rectangle newWindow = new Rectangle(200, 350, 100, 100);
+      e.Graphics.DrawRectangle(blackPen, 200, 350, 100, 100);
+      e.Graphics.FillRectangle(windowBrush, newWindow);
+      e.Graphics.DrawLine(blackPen, 250, 350, 250, 450);
+      e.Graphics.DrawLine(blackPen, 200, 400, 300, 400);
+      Point newTruba1 = new Point(270, 202);
+      Point newTruba2 = new Point(300, 240);
+      Point newTruba3 = new Point(270, 180);
+      Point newTruba4 = new Point(300, 180);
+      SolidBrush grayBrush = new SolidBrush(Color.Gray);
+      Point[] truba = {newTruba1, newTruba2, newTruba4, newTruba3};
+      e.Graphics.DrawPolygon(blackPen, truba);
+      e.Graphics.FillPolygon(grayBrush, truba);
+    }
+    private void button1_Click(object sender, EventArgs e)
+    {
+      SolidBrush grayBrush = new SolidBrush(Color.Gray);
+      if (flag != true)
+      {
+        Rectangle makeSmoke = new Rectangle(268, 160, 35, 38);
+        float startAngle = 0.0F;
+        float sweepAngle = -180.0F;
+        this.CreateGraphics().FillPie(grayBrush, makeSmoke, startAngle, sweepAngle);
+      }
+      else
+      {
+
+      }
     }
   }
 }
