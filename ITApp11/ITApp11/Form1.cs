fffc2e10fc5c8ead115339de3f6fe0729a4bc0ec
kerbal <zebra46@gmail.com>
2021-04-05 09:13:45 +0300

    Работа номер 9

--- ITApp11/ITApp11/Form1.cs
+++ ITApp11/ITApp11/Form1.cs
@@ -17,5 +17,14 @@ namespace ITApp11
     {
       InitializeComponent();
     }
+
+    private void Form1_Paint(object sender, PaintEventArgs e)
+    {
+      Graphics painting = e.Graphics;
+      painting.Clear(Color.White);
+      Pen blackPen = new Pen(Color.Black, 3);
+      Rectangle rect = new Rectangle(0, 0, 200, 100);
+      e.Graphics.DrawRectangle(blackPen, 0, 0, 500, 500);
+    }
   }
 }
